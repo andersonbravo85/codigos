223888601189a801fb550f470623cd4a3656de86
Anderson Bravo <51916365+andersonbravo85@users.noreply.github.com>
2023-01-25 08:14:37 -0300

    Update criptografia_de_senha.php

--- PHP/criptografia_de_senha.php
+++ PHP/criptografia_de_senha.php
@@ -1,14 +1,20 @@
 /*
-  Criando uma senha forte
+  Script de exemplo de como criar e verificar um hash de senha.
+  -------------------------------------------------------------
+
+  password_hash -> cria um password hash 
+  (https://www.php.net/manual/pt_BR/function.password-hash.php)
+  
+  password_verify -> verifica se um password corresponde com um hash
+  (https://www.php.net/manual/pt_BR//function.password-verify.php)
 */
 
 // criação
 $senha = "mamae0";
-
-$senhaforte = password_hash($senha, PASSWORD_DEFAULT);
+$hash = password_hash($senha, PASSWORD_DEFAULT);
 
 // verificação
-if (password_verify($senha, $senhaforte))
+if (password_verify($senha, $hash))
   echo 'Senha correta';
 else
   echo 'Senha incorreta';
