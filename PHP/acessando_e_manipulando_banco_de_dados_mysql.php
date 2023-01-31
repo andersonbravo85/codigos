5a689861a01f06b515cf69002bbf4d38f8250920
Anderson Bravo <51916365+andersonbravo85@users.noreply.github.com>
2023-01-31 09:47:51 -0300

    Update acessando_e_manipulando_banco_de_dados_mysql.php

--- PHP/acessando_e_manipulando_banco_de_dados_mysql.php
+++ PHP/acessando_e_manipulando_banco_de_dados_mysql.php
@@ -1,11 +1,10 @@
 <?php
 
 /*
-
-mysqli - Extensão que permite a manipulação de bancos de dados MYSQL
-
-https://www.php.net/manual/pt_BR/book.mysqli.php
-
+	Exemplos de acesso e manipulação de banco de dados ( MYSQL )
+	
+	mysqli - Extensão que permite a manipulação de bancos de dados MYSQL
+	https://www.php.net/manual/pt_BR/book.mysqli.php
 */
 
 // Parâmetros de conexão ao banco de dados
@@ -17,33 +16,44 @@ $senha = "SENHA_DO_USUARIO";
 // Conecta no banco de dados, retornando um 'ponteiro' com a conexão para os próximos comandos
 $db = mysqli_connect($servidor, $usuario, $senha, $banco);
 
-if (mysqli_connect_errno()) {
-    echo "ERRO: ".mysqli_connect_error();
-    die;
-}
+if (!$db || mysqli_connect_errno())
+    die("ERRO: ".mysqli_connect_error());
+
+//
+// mysqli_query() - Executa o comando sql
+//
 
 // Inicia as transações no banco de dados
 mysqli_query($db, "BEGIN");
 
-$sql = "SELECT nome, idade FROM pessoa;";
+// insere no banco
+$sql = "INSERT INTO pessoa (nome, idade) VALUES ('Anderson', 37);";
 
-// Executa uma query no banco de dados (SELECT, UPDATE, DELETE)
 $query = mysqli_query($db, $sql);
 
 if (!$query) {
-	// Algo deu errado. Termina a transação, ignorando as operações realizadas anteriormente
-	mysqli_query($db, "ROLLBACK");
+	// Algo deu errado. Termina a transação, anulando as operações realizadas anteriormente
+	mysqli_query($db, "ROLLBACK");	
+	die("Error description: " . mysqli_error($db));
 }
 
-// Em caso de SELECT, temos uma query que nos retornará as linhas selecionadas
-// Retorna o número de linhas encontradas
+// retorna o último ID inserido
+$ultimo_id_inserido = mysqli_insert_id($db);
+
+// seleciona dados do banco
+$sql = "SELECT nome, idade FROM pessoa;";
+
+$query = mysqli_query($db, $sql);
+
+// Em caso de SELECT, retorna o número de linhas encontradas
 $numero_de_pessoas = mysqli_num_rows($query);
 
+// Em caso de SELECT, temos uma query que nos retornará as linhas selecionadas
 while ($pessoa = mysqli_fetch_assoc($query))
 	echo "Meu nome é ".$pessoa['nome']." e eu tenho ".$pessoa['idade']." anos.\r\n";
-	
-// Em caso de INSERT
-$ultimo_id_inserido = mysqli_insert_id($db);
+
+// retorna o ponteiro para o primeiro registro do banco
+ mysqli_data_seek($db, 0);
 
 // Finaliza as transações. Nesse momento as operações realizadas são salvas no banco de dados
 mysqli_query($db, "COMMIT");
